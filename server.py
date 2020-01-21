3915f86c5c24a833a20c6bca1a24c710458d6077
Jonathan Ballard <asgardschosen@gmail.com>
2020-01-20 19:49:22 -0800

    initial commit

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect # added request
+app = Flask(__name__)
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+       
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    print(request.form)
+    name_from_form = request.form['name']
+    email_from_form = request.form['email']
+    return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form)
+
+
+@app.route('/result', methods=['POST'])
+def process():
+    print('processing')
+    username = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
+
+    if request.form.get('gender'):
+        gender = request.form['gender']
+    else:
+        gender = "No Reply"
+
+    if request.form.get('newsletter'):
+        # newsletter = request.form['newsletter']
+        newsletter = request.form.get('newsletter')
+        return render_template("result.html", nameTemp = username, locationTemp = location, languageTemp = language, commentTemp = comment, newsletterTemp = newsletter, genderTemp = gender)
+    else:
+        return render_template("result.html", nameTemp = username, locationTemp = location, languageTemp = language, commentTemp = comment, genderTemp = gender)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
